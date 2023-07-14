5959bd2d05805c19392a02c28547e205a8dad96f
Czuprin Rennsport und Technik <109422675+czuprin@users.noreply.github.com>
2023-07-14 14:16:14 +0200

    add

---
+++ IDF3/2/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/czuprin/esp/esp-idf/components/bootloader/subproject"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix/tmp"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix/src"
+  "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/czuprin/Documents/GitHub/CMS/IDF3/2/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
